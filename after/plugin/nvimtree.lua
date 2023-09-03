d1649091e647aa515e9c7510d5ae03639fc9b03d
Yannick Bohrer <yannick.bohrer@collatum-it.de>
2023-09-03 21:47:48 +0200

    removed nvim-tree.config.nvim_tree_callback

--- after/plugin/nvimtree.lua
+++ after/plugin/nvimtree.lua
@@ -20,13 +20,6 @@ if not status_ok then
     return
 end
 
-local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
-if not config_status_ok then
-    return
-end
-
-local tree_cb = nvim_tree_config.nvim_tree_callback
-
 nvim_tree.setup {
     on_attach = my_on_attach,
     disable_netrw = true,
