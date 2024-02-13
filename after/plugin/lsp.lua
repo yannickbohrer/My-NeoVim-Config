30441a6a117425f97a56bbfa6a389124aa91fd7d
Yannick Bohrer <yannick.bohrer@collatum-it.de>
2024-02-13 18:38:27 +0100

    remove unnecessary code from lsp config

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,7 +3,6 @@ local lsp = require('lsp-zero').preset({})
 lsp.ensure_installed({
 	'tsserver',
 	'eslint',
-	'rust_analyzer',
 	'clangd',
 	'pyright',
 	'kotlin_language_server',
@@ -15,14 +14,9 @@ lsp.ensure_installed({
 lsp.on_attach(function(client, bufnr)
 	lsp.default_keymaps({ buffer = bufnr })
 
-	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-	vim.keymap.set("n", "<leader>h", function() vim.lsp.buf.hover() end, opts)
+	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end)
+	vim.keymap.set("n", "<leader>h", function() vim.lsp.buf.hover() end)
 end)
-
-
--- When you don't have mason.nvim installed
--- You'll need to list the servers installed in your system
-lsp.setup_servers({ 'tsserver', 'eslint' })
 
 -- (Optional) Configure lua language server for neovim
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
